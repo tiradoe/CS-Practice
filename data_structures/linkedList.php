d9844a97485f5870082f09aaf34bc9b8fc5b0715
Edward Tirado Jr <ed@etdevelopment.net>
2021-02-20 23:46:35 -0700

    Added PHP linkedlist implementation

---
+++ data_structures/linkedList.php
@@ -0,0 +1,90 @@
+<?php
+class Node
+{
+    public string $data;
+    public ?Node $next = null;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+}
+
+
+class LinkedList
+{
+    private Node $head;
+    private int $length = 0;
+
+    public function insert(Node $newNode)
+    {
+        if ($this->length === 0) {
+            $this->head = $newNode;
+            $this->length++;
+            return;
+        }
+
+        $currentNode = $this->head;
+        while (!empty($currentNode->next)) {
+            $currentNode = $currentNode->next;
+        }
+
+        $currentNode->next = $newNode;
+        $this->length++;
+    }
+
+    public function remove(string $query)
+    {
+        $previousNode = null;
+        $currentNode = $this->head;
+
+        for ($i = 0; $i < $this->length; $i++) {
+            $previousNode = $currentNode;
+
+            if ($currentNode->data === $query) {
+                $previousNode->next = $currentNode->next;
+                $this->length--;
+                unset($currentNode);
+
+                return;
+            }
+
+            $currentNode = $currentNode->next;
+        }
+
+        echo ("$query not found in list.\n");
+    }
+
+    public function display()
+    {
+        $currentNode = $this->head;
+        for ($i = 0; $i < $this->length; $i++) {
+            echo ($currentNode->data . "\n");
+            $currentNode = $currentNode->next;
+        }
+
+        echo ("\n");
+    }
+}
+
+function main()
+{
+    $list = new LinkedList();
+
+    $node1 = new Node("Node One");
+    $list->insert($node1);
+
+    $node2 = new Node("Node Two");
+    $list->insert($node2);
+
+    $node2 = new Node("Node Three");
+    $list->insert($node2);
+    $list->display();
+
+    $list->remove("Node Two");
+    $list->display();
+
+    $list->remove("Node five");
+}
+
+main();
